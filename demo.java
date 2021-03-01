c5aed3ee9c5d147f96d055cf479e0c05bb8d4099
huiyi <huiyi871018@163.com>
2021-03-01 15:25:01 -0500

    初始版本

---
+++ demo.java
@@ -0,0 +1,14 @@
+package test;
+
+import java.util.*;
+
+public class Test {
+
+
+    public static void main(String[] args) {
+        Test t = new Test();
+        int[] nums = {1,2,3};
+
+
+    }
+}
